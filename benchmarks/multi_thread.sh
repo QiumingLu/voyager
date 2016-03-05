78768453dd4b38faafc8efe0b8d9c2b9d5c7cd29
QiumingLu <cming_cs@163.com>
2016-03-05 17:25:25 +0800

    benchmarks

--- benchmarks/multi_thread.sh
+++ benchmarks/multi_thread.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 
 killall server
-timeout=${timeout:-100}
+timeout=100
 bufsize=16384
 
 for nosessions in 100 1000; do
   for nothreads in 1 2 3 4; do
     sleep 5
     echo "Bufsize: $bufsize Threads: $nothreads Sessions: $nosessions"
-    ../../build/debug/bin/server 0.0.0.0 55555 $nothreads & srvpid=$!
-    ../../build/debug/bin/client localhost 55555 $nothreads $bufsize $nosessions $timeout 
+    ../../build/debug/bin/server 127.0.0.1 55555 $nothreads & srvpid=$!
+    ../../build/debug/bin/client 127.0.0.1 55555 $nothreads $bufsize $nosessions $timeout 
     kill -9 $srvpid
   done
 done
--- benchmarks/single_thread.sh
+++ benchmarks/single_thread.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 
 killall server
-timeout=${timeout:-100}
+timeout=100
 bufsize=16384
 nothreads=1
 
 for nosessions in 1 10 100 1000 10000; do
   sleep 5
   echo "Bufsize: $bufsize Threads: $nothreads Sessions: $nosessions"
-  taskset -c 1 ../../build/debug/bin/server 0.0.0.0 55555 $nothreads & srvpid=$!
-  taskset -c 2 ../../build/debug/bin/client 127.0.0.1 55555 $nothreads $bufsize $nosessions $timeout
+  ../../build/debug/bin/server 127.0.0.1 55555 $nothreads & srvpid=$!
+  ../../build/debug/bin/client 127.0.0.1 55555 $nothreads $bufsize $nosessions $timeout
   kill -9 $srvpid
 done
