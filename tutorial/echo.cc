a8ef264c8c7497f020a3bf0b7519f3b19c7120bb
QiumingLu <cming_cs@163.com>
2016-08-13 00:14:21 +0800

    fix bug

--- tutorial/echo.cc
+++ tutorial/echo.cc
@@ -3,7 +3,9 @@
 #include <voyager/core/sockaddr.h>
 #include <voyager/core/tcp_connection.h>
 #include <voyager/core/callback.h>
+#include <voyager/core/newtimer.h>
 #include <voyager/util/logging.h>
+#include <voyager/util/timeops.h>
 #include <unistd.h>
 #include <inttypes.h>
 #include <gperftools/profiler.h>
@@ -47,13 +49,15 @@ class EchoServer {
 // 加入了Google PerfTools来测试,如果不需要可以去掉
 int main(int argc, char** argv) {
   ProfilerStart("MyProfile");
-  voyager::SetLogHandler(NULL);
+  //voyager::SetLogHandler(NULL);
   printf("pid=%d, tid=%" PRIu64"\n",
          getpid(), voyager::port::CurrentThread::Tid());
   voyager::EventLoop ev;
   voyager::SockAddr addr(5666);
   voyager::EchoServer server(&ev, addr);
-  ev.RunAfter(std::bind(&voyager::EventLoop::Exit, &ev), 180*1000000);
+  voyager::NewTimer timer(&ev, std::bind(&voyager::EventLoop::Exit, &ev));
+  timer.SetTime(180 * (voyager::timeops::kNonasSecondsPerSecond), 0);
+  //ev.RunAfter(std::bind(&voyager::EventLoop::Exit, &ev), 180*1000000);
   server.Start();
   ev.Loop();
   ProfilerStop();
