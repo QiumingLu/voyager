0a7cbcdc979f6347a73957cee585eca405e60deb
QiumingLu <mirantslu@gmail.com>
2017-01-17 20:18:34 +0800

    update CMakeLists.txt

--- build.sh
+++ build.sh
@@ -4,12 +4,10 @@ set -x
 
 SYSTEM=`uname -a`
 SOURCE_DIR=`pwd`
-BUILD_DIR=${BUILD_DIR:-../build}
+BUILD_DIR=${BUILD_DIR:-./build}
 BUILD_TYPE=${BUILD_TYPE:-release}
 INSTALL_DIR=${INSTALL_DIR:-../${BUILD_TYPE}-install}
 BUILD_NO_EXAMPLES=${BUILD_NO_EXAMPLES:-0}
-BUILD_RPC=${BUILD_RPC:-0}
-PROTOBUF_PATH=${PROTOBUF_PATH:-/usr/local}
 
 mkdir -p $BUILD_DIR/$BUILD_TYPE \
   && cd $BUILD_DIR/$BUILD_TYPE \
@@ -17,8 +15,6 @@ mkdir -p $BUILD_DIR/$BUILD_TYPE \
            -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
            -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR \
            -DCMAKE_BUILD_NO_EXAMPLES=$BUILD_NO_EXAMPLES \
-           -DCMAKE_BUILD_RPC=$BUILD_RPC \
-           -DCMAKE_PROTOBUF_PATH=$PROTOBUF_PATH \
            $SOURCE_DIR \
   && make \
   && make install
