36476de4bbd26d4a098bd7efbd97d597bd44ec79
QiumingLu <mirantslu@gmail.com>
2017-01-17 17:24:31 +0800

    add rpc

---
+++ voyager/rpc/rpc_server.h
@@ -0,0 +1,38 @@
+#ifndef VOYAGER_RPC_SERVER_H_
+#define VOYAGER_RPC_SERVER_H_
+
+#include <map>
+#include <string>
+#include <google/protobuf/service.h>
+//#include <google/protobuf/stubs/callback.h>
+#include "voyager/core/eventloop.h"
+#include "voyager/core/sockaddr.h"
+#include "voyager/core/tcp_connection.h"
+#include "voyager/core/tcp_server.h"
+#include "voyager/rpc/rpc.pb.h"
+
+namespace voyager {
+
+class RpcServer {
+ public:
+  RpcServer(EventLoop* loop, const SockAddr& addr, int thread_size = 1);
+
+  void Start();
+  void RegisterService(google::protobuf::Service* service);
+
+ private:
+  void OnMessage(const TcpConnectionPtr& p, Buffer* buf);
+  void OnRequest(const TcpConnectionPtr& p, const RpcMessage& msg);
+  void Done(google::protobuf::Message* response, TcpConnectionPtr p);
+
+  TcpServer server_;
+  std::map<std::string, google::protobuf::Service*> services_;
+
+  // No copying allowed
+  RpcServer(const RpcServer&);
+  void operator=(const RpcServer&);
+};
+
+}  // namespace voyager
+
+#endif  // VOYAGER_RPC_SERVER_H_
