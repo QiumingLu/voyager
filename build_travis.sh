43cea2aa98e01e4528637fbe8eb4158c6c493c2e
QiumingLu <mirantslu@gmail.com>
2017-03-05 10:45:16 +0800

    support macOS test

--- build_travis.sh
+++ build_travis.sh
@@ -2,6 +2,17 @@ CXX=g++
 CXXFLAGS="-std=c++11 -pthread -I/usr/local/include -I/usr/include"
 CXXOUTPUT="/tmp/skywalker_build_detect_platform-cxx.$$"
 
+# Detect OS
+if test -z "$TARGET_OS"; then
+    TARGET_OS=`uname -s`
+fi
+
+if [ "$TARGET_OS" = "Darwin" ]; then
+    INSTALL_PATH=/usr/local
+else
+    INSTALL_PATH=/usr
+fi
+
 # Test whether Protobuf library is installed
 $CXX $CXXFLAGS -x c++ - -o $CXXOUTPUT 2>/dev/null  <<EOF
     #include <google/protobuf/arena.h>
@@ -12,7 +23,7 @@ EOF
     tar zxvf v3.2.0.tar.gz
     cd protobuf-3.2.0
     ./autogen.sh
-    ./configure --prefix=/usr
+    ./configure --prefix=$INSTALL_PATH
     make
     sudo make install
     cd ..
