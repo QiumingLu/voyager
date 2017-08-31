d0aab072c86af557e9ca30f57743eb78db75d812
QiumingLu <mirantslu@gmail.com>
2017-08-31 11:53:23 +0800

    fix bug

--- voyager/protobuf/protobuf_codec.h
+++ voyager/protobuf/protobuf_codec.h
@@ -16,7 +16,6 @@
 namespace voyager {
 
 enum ProtoCodecError {
-  kOk,
   kSerializeError,
   kParseError,
 };
@@ -32,20 +31,21 @@ class ProtobufCodec {
   ProtobufCodec() {}
 
   void SetMessageCallback(const MessageCallback& cb) { message_cb_ = cb; }
+  void SetMessageCallback(MessageCallback&& cb) { message_cb_ = std::move(cb); }
   void SetErrorCallback(const ErrorCallback& cb) { error_cb_ = cb; }
+  void SetErrorCallback(ErrorCallback&& cb) { error_cb_ = std::move(cb); }
 
-  bool SendMessage(const TcpConnectionPtr& p, const Message& message) {
+  bool SendMessage(const TcpConnectionPtr& p, const Message& message) const {
     bool res = false;
     if (p) {
+      char buf[kHeaderSize];
+      memset(buf, 0, kHeaderSize);
       std::string s;
-      char buf[kHeaderLen];
-      memset(buf, 0, kHeaderLen);
-      s.append(buf, kHeaderLen);
+      s.append(buf, kHeaderSize);
       res = message.AppendToString(&s);
       if (res) {
-        uint32_t size = static_cast<uint32_t>(s.size());
-        EncodeFixed32(buf, size);
-        s.replace(s.begin(), s.begin() + kHeaderLen, buf, kHeaderLen);
+        EncodeFixed32(buf, static_cast<uint32_t>(s.size()));
+        s.replace(s.begin(), s.begin() + kHeaderSize, buf, kHeaderSize);
         p->SendMessage(std::move(s));
       } else if (error_cb_) {
         error_cb_(p, kSerializeError);
@@ -57,12 +57,12 @@ class ProtobufCodec {
   void OnMessage(const TcpConnectionPtr& p, Buffer* buf) {
     bool res = true;
     while (res) {
-      if (buf->ReadableSize() >= kHeaderLen) {
+      if (buf->ReadableSize() >= kHeaderSize) {
         uint32_t size = DecodeFixed32(buf->Peek());
         if (buf->ReadableSize() >= static_cast<size_t>(size)) {
           std::unique_ptr<Message> message(new Message());
-          res = message->ParseFromArray(buf->Peek() + kHeaderLen,
-                                        size - kHeaderLen);
+          res = message->ParseFromArray(buf->Peek() + kHeaderSize,
+                                        size - kHeaderSize);
           if (res) {
             if (message_cb_) {
               res = message_cb_(p, std::move(message));
@@ -79,7 +79,7 @@ class ProtobufCodec {
   }
 
  private:
-  static const uint32_t kHeaderLen = 4;
+  static const uint32_t kHeaderSize = 4;
 
   MessageCallback message_cb_;
   ErrorCallback error_cb_;
